c75bf1b428dbe93374ba2336f734e1d9d4bf667d
astur <astur@yandex.ru>
2019-10-12 14:35:14 +0300

    option for return seconds

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-module.exports = str => {
+module.exports = (str, sec = false) => {
+    const x = sec ? 1 : 1000;
     if(typeof str !== 'string') return 0;
     const fixed = str.replace(/\s/g, '');
     const tail = +fixed.match(/\d+$/g) || 0;
     const parts = (fixed.match(/\d+\D+/g) || [])
-        .map(v => +v.replace(/\D+/g, '') * ({s: 1000, m: 60000, h: 3600000, d: 86400000}[v.replace(/\d+/g, '')] || 0));
+        .map(v => +v.replace(/\D+/g, '') * ({s: x, m: 60 * x, h: 3600 * x, d: 86400 * x}[v.replace(/\d+/g, '')] || 0));
     return [tail, ...parts].reduce((a, b) => a + b, 0);
 };
