51ac437288dfe0c532d52f73e8da4f07f3c32d91
astur <astur@yandex.ru>
2019-10-12 13:34:21 +0300

    main code refactored

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports = str => (str.replace(/\s/g, '').match(/\d+(\D+|$)/g) || [])
-    .map(v => +v.replace(/\D+/g, '') * ({
-        s: 1000,
-        m: 60000,
-        h: 3600000,
-        d: 86400000,
-    }[v.replace(/\d+/g, '')] || 1))
-    .reduce((a, b) => a + b, 0);
+module.exports = str => {
+    if(typeof str !== 'string') return 0;
+    const fixed = str.replace(/\s/g, '');
+    const tail = +fixed.match(/\d+$/g) || 0;
+    const parts = (fixed.match(/\d+\D+/g) || [])
+        .map(v => +v.replace(/\D+/g, '') * ({s: 1000, m: 60000, h: 3600000, d: 86400000}[v.replace(/\d+/g, '')] || 0));
+    return [tail, ...parts].reduce((a, b) => a + b, 0);
+};
