393c1b2a113597dfe557a1aaa17601b8e4770d9b
astur <astur@yandex.ru>
2019-10-12 12:52:53 +0300

    base code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = () => {};
+module.exports = str => (str.replace(/\s/g, '').match(/\d+(\D+|$)/g) || [])
+    .map(v => +v.replace(/\D+/g, '') * ({
+        s: 1000,
+        m: 60000,
+        h: 3600000,
+        d: 86400000,
+    }[v.replace(/\d+/g, '')] || 1))
+    .reduce((a, b) => a + b, 0);
